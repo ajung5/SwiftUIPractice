818fa4968d045a247ab59c7cd5546a7c4d6be550
Agung Nawawi <agung@fairtech.com.sg>
2024-08-07 11:27:37 +0700

    create imageLoaderView

--- SwiftUIPractice/Shared/ImageLoaderView.swift
+++ SwiftUIPractice/Shared/ImageLoaderView.swift
@@ -11,15 +11,25 @@ import SDWebImageSwiftUI
 struct ImageLoaderView: View {
     
     var urlString: String = Constants.randomImage
+    var resizingMode: ContentMode = .fill
     
     var body: some View {
-        WebImage(url : URL(string: urlString))
-            .resizable()
-            .indicator(.activity)
-            .scaledToFill()
+        Rectangle()
+            .opacity(0.001)
+            .overlay(
+                WebImage(url : URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            )
+            .clipped()
     }
 }
 
 #Preview {
     ImageLoaderView()
+        .clipShape(RoundedRectangle(cornerRadius: 30))
+        .padding(40)
+        .padding(.vertical, 60)
 }
